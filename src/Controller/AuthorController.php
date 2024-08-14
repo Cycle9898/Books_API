15bfe6680cec1f3aadbe7ab23f1dc19e1d64da78
Cycle9898 <cycle9898@gmail.com>
2024-08-14 19:06:43 +0200

    adding endpoint in AuthorController to get details from an author

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
 use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -16,8 +17,16 @@ class AuthorController extends AbstractController
     public function getAuthorsList(AuthorRepository $authorRepo, SerializerInterface $serializer): JsonResponse
     {
         $authorsList = $authorRepo->findAll();
-        $jsonAuthorsList = $serializer->serialize($authorsList, 'json', ['groups' => "getAuthors"]);
+        $jsonAuthorsList = $serializer->serialize($authorsList, 'json', ['groups' => 'getAuthors']);
 
         return new JsonResponse($jsonAuthorsList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{id}', name: 'app_author_detail', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function getDetailAuthor(Author $author, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
+
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
     }
 }
