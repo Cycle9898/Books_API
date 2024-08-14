01516a4fe8012a790f5529561e0edbb4f5d77703
Cycle9898 <cycle9898@gmail.com>
2024-08-14 17:55:39 +0200

    adding endpoint in BookController to get details from a book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,15 +10,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
-#[Route('/api/v1')]
+#[Route('/api/v1/books')]
 class BookController extends AbstractController
 {
-    #[Route('/books', name: 'app_books', methods: ['GET'])]
+    #[Route('', name: 'app_books', methods: ['GET'])]
     public function getBooksList(BookRepository $bookRepo, SerializerInterface $serializer): JsonResponse
     {
         $booksList = $bookRepo->findAll();
         $jsonBooksList = $serializer->serialize($booksList, 'json');
 
         return new JsonResponse($jsonBooksList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{id}', name: 'app_book_detail', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function getDetailBook(Book $book, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonBook = $serializer->serialize($book, 'json');
+
+        return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
     }
 }
