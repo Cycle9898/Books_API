35e9bb9282bcd4af7ae933d48725634c6e8cc8a5
Cycle9898 <cycle9898@gmail.com>
2024-08-15 18:43:48 +0200

    adding POST endpoint to create an author

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -7,8 +7,10 @@ use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/v1/authors')]
@@ -21,6 +23,21 @@ class AuthorController extends AbstractController
         $jsonAuthorsList = $serializer->serialize($authorsList, 'json', ['groups' => 'getAuthors']);
 
         return new JsonResponse($jsonAuthorsList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('', name: 'app_author_create', methods: ['POST'])]
+    public function createAuthor(Request $request, SerializerInterface $serializer, EntityManagerInterface $manager, UrlGeneratorInterface $urlGen): JsonResponse
+    {
+        $author = $serializer->deserialize($request->getContent(), Author::class, 'json');
+
+        $manager->persist($author);
+        $manager->flush();
+
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
+
+        $location = $urlGen->generate('app_author_detail', ['id' => $author->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonAuthor, Response::HTTP_CREATED, ['location' => $location], true);
     }
 
     #[Route('/{id}', name: 'app_author_detail', requirements: ['id' => '\d+'], methods: ['GET'])]
