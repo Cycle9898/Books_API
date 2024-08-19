682e79f776f043386aa20e315a2ee61fbd8399cd
Cycle9898 <cycle9898@gmail.com>
2024-08-19 11:26:02 +0200

    updating fixtures with users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,38 @@ namespace App\DataFixtures;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+
+        // creating 2 fake users. One with no privileges and one with admin privileges
+        $basicUser = new User();
+        $basicUser->setEmail('user@bookapi.com');
+        $basicUser->setRoles(["ROLE_USER"]);
+        $basicUser->setPassword($this->userPasswordHasher->hashPassword($basicUser, 'password123'));
+
+        $manager->persist($basicUser);
+
+        $adminUser = new User();
+        $adminUser->setEmail('admin@bookapi.com');
+        $adminUser->setRoles(["ROLE_ADMIN"]);
+        $adminUser->setPassword($this->userPasswordHasher->hashPassword($adminUser, 'password123'));
+
+        $manager->persist($adminUser);
+
         // creating 20 fake authors with basic first name and last name
         $authorsList = [];
         for ($i = 1; $i <= 20; $i++) {
