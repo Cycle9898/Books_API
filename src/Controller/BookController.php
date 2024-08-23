590091f5dd230e6c42c2f2cc81b119d55a57699c
Cycle9898 <cycle9898@gmail.com>
2024-08-23 19:10:57 +0200

    adding API docs endpoint + setting up general docs for Books endpoints

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -9,6 +9,8 @@ use App\Service\VersioningService;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +26,37 @@ use Symfony\Contracts\Cache\TagAwareCacheInterface;
 #[Route('/api/books')]
 class BookController extends AbstractController
 {
+    /**
+     * This method is used to get all the books
+     * 
+     * @param BookRepository $bookRepo
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param TagAwareCacheInterface $cache
+     * @param VersioningService $versioningService
+     * @return JsonResponse
+     */
+    #[OA\Response(
+        response: 200,
+        description: "Get books list",
+        content: new OA\JsonContent(
+            type: "array",
+            items: new OA\Items(ref: new Model(type: Book::class))
+        )
+    )]
+    #[OA\Parameter(
+        name: "page",
+        in: "query",
+        description: "The number of results per page",
+        schema: new OA\Schema(type: "int")
+    )]
+    #[OA\Parameter(
+        name: "limit",
+        in: "query",
+        description: "The page number",
+        schema: new OA\Schema(type: "int")
+    )]
+    #[OA\Tag(name: "Books")]
     #[Route('', name: 'app_books', methods: ['GET'])]
     public function getBooksList(BookRepository $bookRepo, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache, VersioningService $versioningService): JsonResponse
     {
@@ -50,6 +83,33 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBooksList, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * This method is used to create a new book
+     * 
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $manager
+     * @param UrlGeneratorInterface $urlGen
+     * @param AuthorRepository $authorRepo
+     * @param ValidatorInterface $validator
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
+     */
+    #[OA\Response(
+        response: 201,
+        description: "created a new book",
+        content: new Model(type: Book::class)
+    )]
+    #[OA\RequestBody(content: new OA\JsonContent(
+        type: 'object',
+        properties: [
+            new OA\Property(property: 'title', type: 'string'),
+            new OA\Property(property: 'coverText', type: 'string'),
+            new OA\Property(property: 'idAuthor', type: 'int'),
+            new OA\Property(property: 'comment', type: 'string')
+        ]
+    ))]
+    #[OA\Tag(name: "Books")]
     #[Route('', name: 'app_book_create', methods: ['POST'])]
     #[IsGranted('ROLE_ADMIN', message: 'You do not have sufficient rights to create a book')]
     public function createBook(
@@ -89,6 +149,26 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_CREATED, ['location' => $location], true);
     }
 
+    /**
+     * This method is used to get a book details
+     * 
+     * @param Book $book
+     * @param SerializerInterface $serializer
+     * @param VersioningService $versioningService
+     * @return JsonResponse
+     */
+    #[OA\Response(
+        response: 200,
+        description: "Get a book details",
+        content: new Model(type: Book::class)
+    )]
+    #[OA\Parameter(
+        name: 'id',
+        in: 'path',
+        description: 'ID of the book',
+        schema: new OA\Schema(type: 'int')
+    )]
+    #[OA\Tag(name: "Books")]
     #[Route('/{id}', name: 'app_book_detail', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function getDetailBook(Book $book, SerializerInterface $serializer, VersioningService $versioningService): JsonResponse
     {
@@ -100,6 +180,39 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * This method is used to edit a book
+     * 
+     * @param Request $request
+     * @param Book $currentBook
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $manager
+     * @param AuthorRepository $authorRepo
+     * @param ValidatorInterface $validator
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
+     */
+    #[OA\Response(
+        response: 204,
+        description: "edited a book",
+        content: null
+    )]
+    #[OA\Parameter(
+        name: 'id',
+        in: 'path',
+        description: 'ID of the book',
+        schema: new OA\Schema(type: 'int')
+    )]
+    #[OA\RequestBody(content: new OA\JsonContent(
+        type: 'object',
+        properties: [
+            new OA\Property(property: 'title', type: 'string'),
+            new OA\Property(property: 'coverText', type: 'string'),
+            new OA\Property(property: 'idAuthor', type: 'int'),
+            new OA\Property(property: 'comment', type: 'string')
+        ]
+    ))]
+    #[OA\Tag(name: "Books")]
     #[Route('/{id}', name: 'app_book_update', requirements: ['id' => '\d+'], methods: ['PUT'])]
     #[IsGranted('ROLE_ADMIN', message: 'You do not have sufficient rights to modify a book')]
     public function updateBook(
@@ -139,6 +252,26 @@ class BookController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * This method is used to delete a book
+     * 
+     * @param Book $book
+     * @param EntityManagerInterface $manager
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
+     */
+    #[OA\Response(
+        response: 204,
+        description: "deleted a book",
+        content: null
+    )]
+    #[OA\Parameter(
+        name: 'id',
+        in: 'path',
+        description: 'ID of the book',
+        schema: new OA\Schema(type: 'int')
+    )]
+    #[OA\Tag(name: "Books")]
     #[Route('/{id}', name: 'app_book_delete', requirements: ['id' => '\d+'], methods: ['DELETE'])]
     #[IsGranted('ROLE_ADMIN', message: 'You do not have sufficient rights to delete a book')]
     public function deleteBook(Book $book, EntityManagerInterface $manager, TagAwareCacheInterface $cache): JsonResponse
