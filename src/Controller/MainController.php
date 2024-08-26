5c26c6e95071154dbb4aad4003f0e5e4e9b7e6ad
Cycle9898 <cycle9898@gmail.com>
2024-08-26 16:46:20 +0200

    adding doc for Main endpoint

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
+    #[OA\Response(
+        response: 200,
+        description: "Got welcome message",
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'status', type: 'int'),
+                new OA\Property(property: 'message', type: 'string')
+            ]
+        )
+    )]
     #[Route('/api', name: 'app_main', methods: ['GET'])]
     public function index(): JsonResponse
     {
